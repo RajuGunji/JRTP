5754df013e62936144151013aed447c8d7ca6fff
RajuGunji <gmnraju1999@gmail.com>
2024-10-11 17:59:42 +0530

    Bug-121 has stashed and Bug-122 has commited

--- User.java
+++ User.java
@@ -3,11 +3,7 @@ public class User
    public static void main(String[] args)
    {
       // fix BUG-121
-<<<<<<< Updated upstream
          java.util.List<Emp> l=new ArrayList();
 
       // fix Bug-122
 	 java.util.set<Emp> s=new HashSet();
-=======
-         java.util.List<Emp> l=new ArrayList();
->>>>>>> Stashed changes
