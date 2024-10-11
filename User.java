46dac92cbaf260d279adb10dcfaff78eab440ae4
RajuGunji <gmnraju1999@gmail.com>
2024-10-11 17:49:14 +0530

    user.java commited for the first time to Practice Repo

--- User.java
+++ User.java
@@ -3,7 +3,11 @@ public class User
    public static void main(String[] args)
    {
       // fix BUG-121
+<<<<<<< Updated upstream
          java.util.List<Emp> l=new ArrayList();
 
       // fix Bug-122
 	 java.util.set<Emp> s=new HashSet();
+=======
+         java.util.List<Emp> l=new ArrayList();
+>>>>>>> Stashed changes
