862c97b98209bf402622af5c6ce5309c9579ea4f
RajuGunji <gmnraju1999@gmail.com>
2024-10-11 18:13:18 +0530

    BUG-122 applied for stash and commited to JRTP Repo

--- User.java
+++ User.java
@@ -5,7 +5,12 @@ public class User
       // fix BUG-121
          java.util.List<Emp> l=new ArrayList();
 
+
      // fix Bug-123
 	     int a=23432432;
 		 String x="Jeevan";
 		 java.util.Queue<Emp> q=new PriorityQueue();
+
+     // fix Bug-122
+	    java.util.Map<Emp,Integer> m=new HashMap();
+
