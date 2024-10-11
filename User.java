b452c0458caaa2ccf6ef18aa5adcad90db24378d
RajuGunji <gmnraju1999@gmail.com>
2024-10-11 16:46:22 +0530

    BUG-122 is fixed in User.java

---
+++ User.java
@@ -0,0 +1,9 @@
+public class User
+{
+   public static void main(String[] args)
+   {
+      // fix BUG-121
+         java.util.List<Emp> l=new ArrayList();
+
+      // fix Bug-122
+	 java.util.set<Emp> s=new HashSet();
